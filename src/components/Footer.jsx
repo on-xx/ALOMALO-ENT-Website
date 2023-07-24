31cd4e32861736b4a0b35cdef4dc39f8ef5e60d7
On Choi <62741297+on-xx@users.noreply.github.com>
2023-07-24 20:27:26 +0800

    Update Footer.jsx
    footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
         <div className="footer-content">
           <p>Tel : 02-2088-6653 (Artist), 02-2088-5247 (Actor) </p>
           <p>E-mail : alomalohm@naver.com</p>
-          <p>Address : 서울특별시 강남구 도산대로 37길 5, 별관 5-6층</p>
+          <p>Address : 서울특별시 강남구 도산대로 37길 5, 별관 6층</p>
           <p>© 2023 ALOMALO HUMAIN ENT. ALL RIGHTS RESERVED</p>
         </div>
       </div>
